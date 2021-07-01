ba63a79b0bcc943a644cc8df2c27550054ccd914
garettmorrison <garettm123@gmail.com>
2021-07-01 10:20:11 -0500

    Optimized elimination script

--- ColorMatch/removeIslands.py
+++ ColorMatch/removeIslands.py
@@ -6,8 +6,7 @@ import sys
 import random as r
 import time
 
-
-minGroupSize = 100
+minGroupSize = 25
 
 
 
@@ -61,17 +60,16 @@ print("Finding colors")
 
 outPts = []
 ptChecked = []
-for i in range(iW):
-	outPts.append([0]*iH)
-	ptChecked.append([False]*iH)
-
 
 colors = []
 colCount = 0
 
 #Get every color used
-for y in range(iH):
-	for x in range(iW):
+for x in range(iW):
+	outPts.append([0]*iH)
+	ptChecked.append([False]*iH)
+
+	for y in range(iH):
 		testCol = iPix[(x,y)]
 		if not testCol in colors:
 			colors.append(testCol)
@@ -80,7 +78,7 @@ for y in range(iH):
 
 print(colors)
 
-print("Setting into groups")
+print("Sorting into groups")
 
 pixTotal = iW*iH
 pixCounted = 0
@@ -91,7 +89,7 @@ frac = 0
 for y in range(iH):
 	for x in range(iW):
 		if pixCounted/pixTotal > frac:
-			frac += 0.005
+			frac += 0.001
 			print(str(int(frac*1000)/10) + "%")
 			try:
 				inImg.save("out/" + outString + "/temp.png")
@@ -101,69 +99,74 @@ for y in range(iH):
 		if ptChecked[x][y]: #No double set
 			continue
 
-		# print("-----" + str((x,y)))
-
 		setCol = colors.index(iPix[(x,y)])
 		pixToCheck = [(x,y)]
 
+		#Make new group, init borders and group
 		groupSize = 0
 		groupBorders = []
 		groupPix = []
+		borderTallies = [0]*colCount
 
-		borderCounts = [0]*colCount
 		# print("------------------------------------------------------------------------------------------------")
-		while len(pixToCheck) > 0:
-			# print(len(pixToCheck))
+		while len(pixToCheck) > 0: #Loop until all pix in group have been checked
+			if len(pixToCheck) > 200:
+				print(len(pixToCheck))
+
 			pos = pixToCheck.pop(0)
 			fooCol = outPts[pos[0]][pos[1]]
 
-			if fooCol != setCol:
-				# print("Border " + str(fooCol) + " " + str(setCol))
-				groupBorders.append(pos)
-				borderCounts[fooCol] += 1
-				continue
-
-			if ptChecked[pos[0]][pos[1]]: continue #Dont add if already part of group
-
-			# print("Inside " + str(fooCol) + " " + str(setCol))
-
 			groupPix.append(pos)
 			groupSize += 1
+
 			ptChecked[pos[0]][pos[1]] = True
 			pixCounted += 1
 
 			checkPos = ((-1,0), (0,-1), (1,0), (0,1))
 			for i in checkPos:
-				xTest = pos[0] +i[0]
-				yTest = pos[1] +i[1]
-				if not inRange((xTest, yTest), size): continue #Dont add if outside of image
-				if (xTest, yTest) in pixToCheck: continue #Dont add twice
-				if (xTest, yTest) in groupBorders: continue #Dont add twice
+				testPt = (pos[0] +i[0], pos[1] +i[1])
 
-				pixToCheck.append((xTest, yTest))
+				if not inRange(testPt, size): continue #Dont add if outside of image
+				if testPt in pixToCheck: continue #Dont add twice if already on queue
+				if testPt in groupBorders: continue #Dont add twice if already in border
+				if testPt in groupPix: continue #Dont add twice if already in group
+
+				if outPts[testPt[0]][testPt[1]] != setCol or ptChecked[testPt[0]][testPt[1]]:
+					groupBorders.append(testPt)
+					borderTallies[outPts[testPt[0]][testPt[1]]] += 1
+					continue
+
+				# if ptChecked[pos[0]][pos[1]]: continue
+
+				pixToCheck.append(testPt)
 
 		# print("Made Group " + str(setGroup) + " at pos " + str((x,y)) + " with size " + str(groupSize) + " and color " + str(matchCol), end="")
 
 		if groupSize > minGroupSize:
-		# 	print("")
 			continue
-
-		# print(" and then pruned")
 
 		adjacentGroupCounts = []
 		adjacentGroupCols = []
 
-		newColIndex = borderCounts.index(max(borderCounts))
+		newColIndex = borderTallies.index(max(borderTallies))
+		newCol = colors[newColIndex]
+
 		# print("SET " + str(setCol) + " val " + str(colors[setCol]))
-		# print("NEW " + str(newColIndex) + " val " + str(colors[newColIndex]))
-		# print(borderCounts)
+		# print("NEW " + str(newColIndex) + " val " + str(newCol))
+		# print(borderTallies)
 		# print(str(newColIndex) + " ")
 		# print(groupBorders)
 
 		for pos in groupPix:
-			iPix[pos] = colors[newColIndex]
+			ptChecked[pos[0]][pos[1]] = False
+			pixCounted -= 1
+			iPix[pos] = newCol
+			# print("AAAAA")
 			outPts[pos[0]][pos[1]] = newColIndex
 
+		# inImg.save("out/" + outString + "/temp.png")
+		# sys.exit()
 
 
 inImg.save(imgPath)
+# inImg.save("out/" + outString + "/temp.png")
